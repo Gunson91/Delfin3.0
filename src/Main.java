a7af4d1ea7066164f09df21b032661bd1fdd22bb
Nademtis <Mathias.99.madsen@gmail.com>
2022-12-07 13:24:55 +0100

    onsdags loot

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         new Main().run();
+        //test
     }
 
     private void run() throws IOException {
